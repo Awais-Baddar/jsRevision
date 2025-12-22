7b65e14e5a1f355d579db6408085cc2ae58fe357
Awais-Baddar <m.awaisbaddar@gmail.com>
2025-12-22 05:31:01 +0000

    12/22/2025

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,28 @@
-console.log("Just revising few topics")
+// Variable Declaration
+
+// let 
+// const
+// var  ->  we dont use it due to block and fuction scope
+
+
+// JavaScript data types:
+// Number.
+// String.
+// Boolean.
+// Null.
+// Undefined.
+// BigInt.
+// Symbol.
+// Object
+
+
+//data type conversion
+
+// toString
+//Number(target)
+//NaN
+
+
+
+// funcitons
+// typeof
