7626e6e0028a41995781c11b82d1a975ff93099f
Awais-Baddar <m.awaisbaddar@gmail.com>
2025-12-25 18:42:12 +0000

    25th dec

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -26,3 +26,93 @@
 
 // funcitons
 // typeof
+
+// let score  = true;
+// console.log(typeof score);
+
+// let score2  = "33abc";
+// console.log(typeof score2);
+
+// let score3 = {a:1};
+// console.log(typeof score3);
+
+
+//to convert 
+
+// let valueInNum = Number(score2)
+// console.log(valueInNum);
+// console.log(typeof valueInNum);
+
+
+// let valueinStr = score.toString()
+// console.log(valueinStr);
+// console.log(typeof valueinStr);
+
+// let InBoolean= Boolean ()
+
+//NaN has type of number but value can be different than number like 22abc
+
+
+
+
+//operations 
+
+// ++ ,- , plus minus etc
+
+//comparison and equality
+// ==  comapres value , === compares datatype as well 
+
+
+
+// summary 
+
+// // primitive datatypes 
+// String number boolean null undefined symbol BigInt
+
+// non primitive (call by reference )
+// array . object . functions 
+
+
+// types of memories  -->  heap . stack .
+
+
+//Strings
+
+
+// .concat() , + to do same thing , ` backtics` used for string interpolation  `${score} is the best number 
+// const string1 = new String(' string ')
+//cahrAt. lowercase, upprercase , indexOf , valueOf
+//substring, slice, splice  , split, trim , includes 
+
+//numbers , maths, maths.random . floor , ceil , 
+
+
+//array push pop unshift shift spread ... , array.flat(depth e.g infinty)
+
+
+
+//objects
+
+// const juser={
+//     name:'awais',
+//     age:12
+// }
+
+//can be accessed by dot . operator  or can be done by [] e.g juser[age], .freeze to avoid changes , obj.assign()
+// object.keys, object.values, object.entries
+
+//hasOwnProperty
+
+
+//DESTRUCTURING OF VALUES ;
+
+//
+//   const course ={
+//     courseName: 'ssss',
+// coursePrice:2223,
+// coursedays:['monday','tuesday',['monday','tuesday',['monday','tuesday']]]
+// }
+
+// const {coursedays : days} = course;
+// console.log(days.flat(Infinity
+// ));
